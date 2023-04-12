dc56bd1ca8f392d9f49c8eae889ce4f7f1b50795
SHIV <cu.gagankashyap@gmail.com>
2023-04-12 22:49:20 +0530

    payment model&migration added

---
+++ database/migrations/2023_04_12_170109_create_new_table_thats_name_payments.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNewTableThatsNamePayments extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('booking_id');
+            $table->string('booking_ref_id');
+            $table->float('base_price')->default(0.00);
+            $table->float('booking_charge')->default(0.00);
+            $table->float('sms_charge')->default(0.00);
+            $table->float('cancellation_charge')->default(0.00);
+            $table->float('discount_amount')->default(0.00);
+            $table->float('total_price')->default(0.00);
+            $table->integer('status')->default(2)->comments('1 For Complete, 2 For In-Complete, 3 For In-Process');
+            $table->string('transaction_id')->nullable(true);
+            $table->string('payment_method')->nullable(true);
+            $table->float('paid_amount')->default(0.00);
+            $table->dateTime('payment_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
